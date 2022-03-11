fcd3864c9dd9880edbeccf3afe9c7852756513f1
33641 <33641@10.12.85.57>
2022-03-11 14:36:12 +0100

    Licence

--- src/main/java/tp10/metier/Action.java
+++ src/main/java/tp10/metier/Action.java
@@ -1,13 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2022 33641.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package tp10.metier;
 
 /**
  *
- * @author perussel
+ * @author Jiayin LI
  */
+
 public abstract class Action {
 
     private String nom;
@@ -24,7 +36,12 @@ public abstract class Action {
     public Action(String nom) {
         this.nom = nom;
     }
-
+    
+    /**
+     * 
+     * @param j
+     * @return the value of the share for the givent day
+     */
     public abstract float valeur(Jour j);
 
     @Override
